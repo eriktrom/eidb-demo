729f67819fc384ecc6ea09732b7ff052ec8457c3
Erik Trom <erik.trom@gmail.com>
2013-10-05 14:28:41 -0700

    add eidb to bower; remove equal test_helper;

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -9,10 +9,10 @@ function exists(selector) {
   return !!find(selector).length;
 }
 
-function equal(actual, expected, message) {
-  message = message || QUnit.jsDump.parse(expected) + " expected but was " + QUnit.jsDump.parse(actual);
-  QUnit.equal.call(this, expected, actual, message);
-}
+// function equal(actual, expected, message) {
+//   message = message || QUnit.jsDump.parse(expected) + " expected but was " + QUnit.jsDump.parse(actual);
+//   QUnit.equal.call(this, expected, actual, message);
+// }
 
 window.exists = exists;
 window.equal = equal;
